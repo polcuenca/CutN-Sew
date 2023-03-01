1595074a9781c8edf237d1de6698620da7689e27
polcuenca <115073490+polcuenca@users.noreply.github.com>
2023-03-01 20:30:24 +0800

    upload

---
+++ CutnSew/connect.php
@@ -0,0 +1,21 @@
+<?php
+    $p = $_POST["p"]
+    if($p == "Submit"){
+    $First_Name = $_POST['First_Name'];
+    $Last_Name = $_POST['Last_Name'];
+    $Age = $_POST['Age'];
+    $Sex = $_POST['Sex'];
+    $Email = $_POST['Email'];
+    $Username = $_POST['Username'];
+    $Password = $_POST['Password'];
+    $Adress = $_POST['Adress'];
+    $Number = $_POST['Number'];
+    }
+    
+
+    //Database Connection
+
+    
+    
+
+?>
